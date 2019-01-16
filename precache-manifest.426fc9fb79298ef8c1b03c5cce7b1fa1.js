183379d474b3e823cded3db36a6ea8a5e917ce36
Alexandr Zhuk <sanek201dg@ukr.net>
2019-01-16 18:20:25 +0200

    Updates

---
+++ precache-manifest.426fc9fb79298ef8c1b03c5cce7b1fa1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebb413bdbd2d1169d012599072e454fc",
+    "url": "/WeatherApp/static/media/ss.ebb413bd.png"
+  },
+  {
+    "revision": "c0e27aa768e1503c4bb536d8ec759ea3",
+    "url": "/WeatherApp/static/media/backround.c0e27aa7.jpg"
+  },
+  {
+    "revision": "3bb8a0205acacaf255df96db4a48304b",
+    "url": "/WeatherApp/static/media/Dzhul.3bb8a020.jpg"
+  },
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "71f967253775d9dcebc5",
+    "url": "/WeatherApp/static/js/main.71f96725.chunk.js"
+  },
+  {
+    "revision": "64c528084ea5938e8017",
+    "url": "/WeatherApp/static/js/1.64c52808.chunk.js"
+  },
+  {
+    "revision": "71f967253775d9dcebc5",
+    "url": "/WeatherApp/static/css/main.c8f0cb34.chunk.css"
+  },
+  {
+    "revision": "64c528084ea5938e8017",
+    "url": "/WeatherApp/static/css/1.eb284832.chunk.css"
+  },
+  {
+    "revision": "f1a488b1adfcd175821c81c54ed633b0",
+    "url": "/WeatherApp/index.html"
+  }
+];
